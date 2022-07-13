06c8d8fea1a48f86a9dddab977a0fa4a6f60dc45
LidiaGarrigo <garrigolidia@gmail.com>
2022-07-13 21:17:42 +0200

    spaces

--- projects/lidias-navigation-bar/src/lib/lidias-navigation-bar.component.ts
+++ projects/lidias-navigation-bar/src/lib/lidias-navigation-bar.component.ts
@@ -1,22 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'lidias-navigation-bar',
   template: `
   <nav class="navbar is-info is-fixed-top"
   role="navigation"
   aria-label="main navigation">
-
-<div class="navbar-brand">
-<a class="navbar-item" id="logo" href="">
+  <div class="navbar-brand">
+  <a class="navbar-item" id="logo" href="">
   <img src="https://lidiagarrigo.com/assets/logo.png" width="60">
-</a>
+  </a>
   <div class="navbar-start" id="inicio">
     <a class="navbar-item has-text-white" routerLink="/home">
       Inicio
     </a>
   </div>
-<a role="button"
+  <a role="button"
     class="navbar-burger"
     aria-label="menu"
     aria-expanded="false"
@@ -26,19 +24,19 @@ import { Component, OnInit } from '@angular/core';
   <span aria-hidden="true "></span>
   <span aria-hidden="true"></span>
   <span aria-hidden="true"></span>
-</a>
-</div>
-<div id="navMenu" class="navbar-menu" >
-<div class="navbar-start">
+  </a>
+  </div>
+  <div id="navMenu" class="navbar-menu" >
+  <div class="navbar-start">
   <a class="navbar-item" routerLink="/about">
     Sobre mí
   </a>
   <a class="navbar-item" routerLink="/contact">
     Contacto
   </a>
-</div>
-</div>
-</nav>
+  </div>
+  </div>
+  </nav>
   `,
   styles: [`
   @import "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css";
